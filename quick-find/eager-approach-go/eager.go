c6ea362ba29194ae3866785546b2d8bd0f280d13
leogtzr <leogutierrezramirez@gmail.com>
2018-08-15 22:30:56 -0600

    Eager implementation.

--- quick-find/eager-approach-go/eager.go
+++ quick-find/eager-approach-go/eager.go
@@ -12,13 +12,55 @@ func (uf UF) Connected(p, q int) bool {
 	return uf.ID[p] == uf.ID[q]
 }
 
-// Union
+// Union ...
 func (uf UF) Union(p, q int) {
+	pID := uf.ID[p]
+	qID := uf.ID[q]
+	for i := 0; i < len(uf.ID); i++ {
+		if uf.ID[i] == pID {
+			uf.ID[i] = qID
+		}
+	}
 
 }
 
+// New creates a UF ...
+func New(n int) UF {
+	i := 0
+	uf := UF{ID: make([]int, n)}
+	for ; i < n; i++ {
+		uf.ID[i] = i
+	}
+	return uf
+}
+
 func main() {
-	uf := UF{ID: make([]int, 5)}
+	uf := New(10)
+
+	uf.Union(4, 3)
 	fmt.Println(uf)
-	fmt.Println(uf.Connected(2, 3))
+
+	uf.Union(3, 8)
+	fmt.Println(uf)
+
+	uf.Union(6, 5)
+	fmt.Println(uf)
+
+	uf.Union(9, 4)
+	fmt.Println(uf)
+
+	uf.Union(2, 1)
+	fmt.Println(uf)
+
+	uf.Union(8, 9)
+	fmt.Println(uf)
+
+	uf.Union(5, 0)
+	fmt.Println(uf)
+
+	uf.Union(7, 2)
+	fmt.Println(uf)
+
+	uf.Union(6, 1)
+	fmt.Println(uf)
 }
